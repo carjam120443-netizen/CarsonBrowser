96183470fe9eb2a726bd13394faf6fa5383c3a54
carjam120443-netizen <carjam120443@gmail.com>
2026-09-19 13:57:39 -0400

    Add CarsonBrowser Android base files

---
+++ app/src/main/java/com/carsonbrowser/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.carsonbrowser
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this).apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl("https://www.google.com")
+        }
+
+        setContentView(webView)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
